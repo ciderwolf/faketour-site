7c0074ab3fb80d478782f1a0a9d0cb7a5bd78b37
bsiderowf <bsiderowf@gmail.com>
2018-11-06 18:56:13 -0600

    Split standings by format

--- matches/standings/getStandings.php
+++ matches/standings/getStandings.php
@@ -12,21 +12,28 @@
 		while($row = $result->fetch_assoc()) {
 			$pOne = $row["player_one"];
 			$pTwo = $row["player_two"];
+			$format = $row["format"];
 			if(!isset($output[$pOne])) {
-				$output[$pOne] = array("constructed" => array("wins" => 0, "losses" => 0), "sealed" => array("wins" => 0, "losses" => 0));
+				$output[$pOne] = array("constructed" => emptyResultObj(), "sealed" => emptyResultObj());
 			}
 			if(!isset($output[$pTwo])) {
-				$output[$pTwo] = array("constructed" => array("wins" => 0, "losses" => 0), "sealed" => array("wins" => 0, "losses" => 0));
+				$output[$pTwo] = array("constructed" => emptyResultObj(), "sealed" => emptyResultObj());
 			}
 			$scores = explode("-", $row["record"]);
-			$winner = $pTwo;
+
 			if((int)$scores[0] > (int)$scores[1]) {
-				$winner = $pOne;
+				$output[$pOne][$format]["matches"]["wins"] += 1;
+				$output[$pTwo][$format]["matches"]["losses"] += 1;
 			}
-			$output[$pOne][$row["format"]]["wins"] += (int)$scores[0];
-			$output[$pOne][$row["format"]]["losses"] += (int)$scores[1];
-			$output[$pTwo][$row["format"]]["wins"] += (int)$scores[1];
-			$output[$pTwo][$row["format"]]["losses"] += (int)$scores[0];
+			else if((int)$scores[0] < (int)$scores[1]) {
+				$output[$pOne][$format]["matches"]["losses"] += 1;
+				$output[$pTwo][$format]["matches"]["wins"] += 1;
+			}
+
+			$output[$pOne][$format]["games"]["wins"] += (int)$scores[0];
+			$output[$pOne][$format]["games"]["losses"] += (int)$scores[1];
+			$output[$pTwo][$format]["games"]["wins"] += (int)$scores[1];
+			$output[$pTwo][$format]["games"]["losses"] += (int)$scores[0];
 		}
 		unset($output[""]);
 		echo json_encode($output);
@@ -34,4 +41,8 @@
 		echo "0 results";
 	}
 	$conn->close();
+
+	function emptyResultObj() {
+		return array("games" => array("wins" => 0, "losses" => 0), "matches" => array("wins" => 0, "losses" => 0));
+	}
 ?>
--- matches/standings/loadBrackets.js
+++ matches/standings/loadBrackets.js
@@ -3,7 +3,6 @@ let players = {};
 const formats = ["constructed", "sealed"];
 getData();
 
-
 function getData() {
 	let request = new XMLHttpRequest();
 	request.open("GET", "getStandings.php", true);
@@ -21,6 +20,67 @@ function getData() {
 
 function createStandings(data) {
 	let players = JSON.parse(data);
+	let sealedRows = {};
+	let constructedRows = {};
+	let totalRows = {};
+	for(playerName of Object.keys(players)) {
+		let player = players[playerName];
+		sealedRows[playerName] = createFormatRow(player, "sealed");
+		constructedRows[playerName] = createFormatRow(player, "constructed");
+		totalRows[playerName] = createTotalRow(player);
+	}
+	
+	let light = false;
+	let sealedTable = document.getElementById("sealed");
+	for(playerName of createFormatOrder(players, "sealed")) {
+		if(light) {
+			sealedRows[playerName].classList.add("light");
+		}
+		light = !light;
+		sealedTable.appendChild(sealedRows[playerName]);
+	}
+	light = false;
+	let constructedTable = document.getElementById("constructed");
+	for(playerName of createFormatOrder(players, "constructed")) {
+		if(light) {
+			constructedRows[playerName].classList.add("light");
+		}
+		light = !light;
+		constructedTable.appendChild(constructedRows[playerName]);
+	}
+	light = false;
+	let totalTable = document.getElementById("total");
+	for(playerName of createTotalOrder(players)) {
+		if(light) {
+			totalRows[playerName].classList.add("light");
+		}
+		light = !light;
+		totalTable.appendChild(totalRows[playerName]);
+	}
+
+	document.getElementById("initial").click();
+
+}
+
+function createFormatRow(player, format) {
+	let playerRow = document.createElement("tr");
+	playerRow.classList.add("standing");
+	let name = document.createElement("td");
+	name.innerHTML = playerName;
+	name.style.color = "orange";
+	let matchScore = document.createElement("td");
+	matchScore.innerHTML = player[format].matches.wins + "-" + player[format].matches.losses;
+	matchScore.classList.add("cell");
+	let gameScore = document.createElement("td");
+	gameScore.innerHTML = player[format].games.wins + "-" + player[format].games.losses;
+	gameScore.classList.add("cell");
+	playerRow.appendChild(name);
+	playerRow.appendChild(matchScore);
+	playerRow.appendChild(gameScore);
+	return playerRow;
+}
+
+function createFormatOrder(players, format) {
 	let playerOrder = [];
 	let playerNames = Object.keys(players);
 	let playerCount = playerNames.length;
@@ -28,7 +88,7 @@ function createStandings(data) {
 		let greatestRecord = 0;
 		let winner = "";
 		for(player of playerNames) {
-			let playerRecord = (players[player].constructed.wins + players[player].sealed.wins)/(players[player].constructed.losses + players[player].sealed.losses);
+			let playerRecord = players[player][format].games.wins/players[player][format].games.losses;
 			if(playerRecord > greatestRecord) {
 				greatestRecord = playerRecord;
 				winner = player;
@@ -37,40 +97,57 @@ function createStandings(data) {
 		playerOrder.push(winner);
 		playerNames.splice(playerNames.indexOf(winner), 1);
 	}
-
-	let standings = document.getElementById("standings");
-	let light = false;
-	for(playerName of playerOrder) {
-		let playerObj = players[playerName];
-		let player = document.createElement("tr");
-		player.classList.add("standing");
-		if(light) {
-			player.classList.add("light");
-		}
-		light = !light;
-		let name = document.createElement("td");
-		name.innerHTML = playerName;
-		name.style.color = "orange";
-		let standard = document.createElement("td");
-		standard.innerHTML = playerObj.constructed.wins + "-" + playerObj.constructed.losses;
-		standard.classList.add("cell");
-		let sealed = document.createElement("td");
-		sealed.innerHTML = playerObj.sealed.wins + "-" + playerObj.sealed.losses;
-		sealed.classList.add("cell");
-		let total = document.createElement("td");
-		total.innerHTML = playerObj.sealed.wins + playerObj.constructed.wins + "-" + (playerObj.sealed.losses + playerObj.constructed.losses);
-		total.classList.add("cell");
-		player.appendChild(name);
-		player.appendChild(standard);
-		player.appendChild(sealed);
-		player.appendChild(total);
-
-		standings.appendChild(player);
-	}
+	return playerOrder;
 }
 
+function createTotalOrder(players) {
+	let playerOrder = [];
+	let playerNames = Object.keys(players);
+	let playerCount = playerNames.length;
+	while(playerOrder.length < playerCount) {
+		let greatestRecord = 0;
+		let winner = "";
+		for(player of playerNames) {
+			let playerRecord = (players[player].constructed.games.wins + players[player].sealed.games.wins)/(players[player].constructed.games.losses + players[player].sealed.games.losses);
+			if(playerRecord > greatestRecord) {
+				greatestRecord = playerRecord;
+				winner = player;
+			}
+		}
+		playerOrder.push(winner);
+		playerNames.splice(playerNames.indexOf(winner), 1);
+	}
+	return playerOrder;
+}
 
+function createTotalRow(player) {
+	let playerRow = document.createElement("tr");
+	playerRow.classList.add("standing");
+	let name = document.createElement("td");
+	name.innerHTML = playerName;
+	name.style.color = "orange";
+	let matchScore = document.createElement("td");
+	matchScore.innerHTML = player.sealed.matches.wins + player.constructed.matches.wins + "-" + (player.sealed.matches.losses + player.constructed.matches.losses);
+	matchScore.classList.add("cell");
+	let gameScore = document.createElement("td");
+	gameScore.innerHTML = player.sealed.games.wins + player.constructed.games.wins + "-" + (player.sealed.games.losses + player.constructed.games.losses);
+	gameScore.classList.add("cell");
+	playerRow.appendChild(name);
+	playerRow.appendChild(matchScore);
+	playerRow.appendChild(gameScore);
+	return playerRow;
+}
 
-
-
-
+function showTab(evt, tab) {
+    let tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
